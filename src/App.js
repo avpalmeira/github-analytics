15ad1ec87d8e5fe2ca0769b41ca795f01f199dc8
Antonio Palmeira <av.palmeira@gmail.com>
2020-11-09 16:49:50 -0300

    fetch data from github api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const githubApiUrl = process.env.REACT_APP_GITHUB_API_URL;
+  const accessToken = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
+  const api = axios.create({
+    baseURL: githubApiUrl,
+    headers: { authorization: `Bearer ${accessToken}` }
+  });
+
+  const [ login, setLogin ] = useState("");
+
+  useEffect(() => {
+    const fetchViewerLogin = async () => {
+      const response = await api.post('/', { query: "query { viewer { login } }" });
+      setLogin(response.data.data.viewer.login);
+    }
+    fetchViewerLogin();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Querying data from the Github API:</p>
+        <p>Username - {login}</p>
       </header>
     </div>
   );
